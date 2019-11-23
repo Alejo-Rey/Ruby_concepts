ac8018fb7affde8f2a4de91c1fb48bde291180d9
Alejo-Rey <771@holbertonschool.com>
2019-11-23 10:12:11 -0500

    [FEATURE] Manipulate integers type

---
+++ Numbers.rb
@@ -0,0 +1,26 @@
+
+## operations
+p 5 + 9 #add
+p 2 * 3 #multiplication
+p 2 ** 3 #pow
+p 10 % 3 #module
+
+num = -10.6
+## inter to string
+p ("number is " + num.to_s)
+
+## absolute
+print "the absolute number is : "
+p num.abs
+
+## round
+print "the round number is : "
+p num.round
+
+## ceil, round to the hightest number
+print "the ceil is : "
+p num.ceil
+
+## floor, the smalest number""
+print "the smaller number : "
+p num.floor
