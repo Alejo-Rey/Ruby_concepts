ce4fc22278a40b22eeeeb1344b6a862118e13b4f
Alejo-Rey <771@holbertonschool.com>
2019-11-23 09:52:59 -0500

    [FEATURE]  types and methods of arrays

---
+++ Arrays.rb
@@ -0,0 +1,13 @@
+names = %w(orange apple banana lemon)
+
+## include, check is there are any string in the array
+puts names.include? "banana"
+
+## reverse
+p "1 Reverse :"
+puts names.reverse
+
+## sort, with only a type if the data
+p "2 Sort : "
+puts names.sort
+
