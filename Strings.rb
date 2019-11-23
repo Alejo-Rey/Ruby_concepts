f07571151dc520a982df40fad1ab85ce814f33d7
Alejo-Rey <771@holbertonschool.com>
2019-11-23 10:17:16 -0500

    [FEATURE]  how to manipulate strings

---
+++ Strings.rb
@@ -0,0 +1,19 @@
+## strings
+
+phrase = "    Example of String   "
+
+## upcase
+puts phrase.upcase
+
+## downcase
+puts phrase.downcase
+
+## strip to dekete de spaces befor and after
+puts phrase.strip
+
+## include to check string inside
+puts phrase.include? "of"
+
+## positions
+puts phrase[0,7]
+puts phrase.index("of")
